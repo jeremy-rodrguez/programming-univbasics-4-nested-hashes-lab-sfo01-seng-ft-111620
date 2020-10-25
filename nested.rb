88c88d7494a8336b31e85c50e378445ccf99fd01
Jeremy Rodriguez <jeremy.rodrguez@yahoo.com>
2020-10-25 04:05:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,10 @@ def hopper
         :languages => ["C"]
       }
     }
-  p programmer_hash[:grace_hopper][:known_for]
+  
 end
+
+programmer_hash[:grace_hopper][:known_for]
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
